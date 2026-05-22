f799a2e5a64f2fab44d9f5114d605089a8c587ad
kot2d <kot2d@outlook.com>
2026-05-22 20:26:54 +0300

    update

---
+++ tiledEditor/port.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.12.1" name="port" tilewidth="32" tileheight="32" tilecount="16" columns="4">
+ <image source="../aseprite/bacground/antibarium/port.png" width="128" height="128"/>
+</tileset>
