d5324dc1eb08abdd1cc7f0470b5d2fa9a4a9816a
kot2d <kot2d@outlook.com>
2026-06-12 12:25:19 +0300

    добавил в рефлексию игровые классы

--- game-client/teavm/src/main/java/com/konivan/teavm/TeaVMBuilder.java
+++ game-client/teavm/src/main/java/com/konivan/teavm/TeaVMBuilder.java
@@ -59,7 +59,11 @@ public class TeaVMBuilder {
             .addReflectionClass("сom.konivan.render.ZComparator")
             .addReflectionClass("com.konivan.components.TransformComponent")
             .addReflectionClass("com.konivan.components.TextureComponent")
+            .addReflectionClass("com.konivan.components.CamerTargetComponent")
             .addReflectionClass("com.konivan.systems.RenderSystem")
+            .addReflectionClass("com.konivan.systems.CameraSystem")
+            .addReflectionClass("com.konivan.characters.Character")
+            .addReflectionClass("com.konivan.characters.Player")
             .build(new File("build/dist"));
     }
 }
