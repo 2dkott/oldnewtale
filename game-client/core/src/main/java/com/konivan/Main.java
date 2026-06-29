572ba7cb43f85ca8f0642c3919cd8934d5cf58cb
kot2d <kot2d@outlook.com>
2026-06-29 22:51:16 +0300

    refactor for variables

--- game-client/core/src/main/java/com/konivan/Main.java
+++ game-client/core/src/main/java/com/konivan/Main.java
@@ -1,108 +1,85 @@
 package com.konivan;
 
-import com.badlogic.ashley.core.Engine;
 import com.badlogic.ashley.core.PooledEngine;
 import com.badlogic.gdx.Game;
 import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.InputMultiplexer;
-import com.badlogic.gdx.InputProcessor;
 import com.badlogic.gdx.graphics.FPSLogger;
 import com.badlogic.gdx.graphics.GL20;
-import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.profiling.GLProfiler;
 import com.badlogic.gdx.maps.tiled.TiledMap;
 import com.badlogic.gdx.maps.tiled.TmxMapLoader;
 import com.badlogic.gdx.math.Vector2;
+import com.badlogic.gdx.math.Vector3;
 import com.badlogic.gdx.physics.box2d.World;
-import com.badlogic.gdx.utils.viewport.ExtendViewport;
-import com.konivan.input.KeyboardInputController;
-import com.konivan.input.PlayTimeControllerState;
+import com.konivan.camera.GameCameraImpl;
+import com.konivan.characters.CharacterCreator;
+import com.konivan.input.InputManager;
 import com.konivan.systems.*;
 
-/** {@link com.badlogic.gdx.ApplicationListener} implementation shared by all platforms. */
+/**
+ * {@link com.badlogic.gdx.ApplicationListener} implementation shared by all
+ * platforms.
+ */
 public class Main extends Game {
 
-    public static final float WORLD_HEIGHT = 9f ;
-    public static final float WORLD_WIDTH = 16f;
-    public static final float UNIT_SCALE = 1f / 64f;
+	public static final float WORLD_HEIGHT = 9f;
+	public static final float WORLD_WIDTH = 16f;
 
-    private OrthographicCamera camera;
-    private Engine engine;
-    private TiledMap map;
+	private TiledMap map;
 
-    private ExtendViewport viewport;
+	private FPSLogger fpsLogger;
 
-    InputMultiplexer inputMultiplexer;
+	private GLProfiler glProfiler;
 
-    FPSLogger fpsLogger;
+	@Override
+	public void create() {
 
-    GLProfiler glProfiler;
-    private KeyboardInputController keyboardController;
+		var engine = new PooledEngine();
 
-    private World physicWorld;
+		var inputManager = new InputManager(engine);
+		Gdx.input.setInputProcessor(inputManager.getInputMultiplexer());
 
-    @Override
-    public void create() {
+        var gameCamera = new GameCameraImpl(WORLD_WIDTH, WORLD_HEIGHT);
 
-        inputMultiplexer = new InputMultiplexer();
-        Gdx.input.setInputProcessor(inputMultiplexer);
+		gameCamera.setPosition(
+				new Vector3(gameCamera.getCamera().viewportWidth / 2, gameCamera.getCamera().viewportHeight / 2, 0));
 
-        var sb = new SpriteBatch();
+		map = new TmxMapLoader().load("maps/towns/first_town/first-town.tmx");
 
-        map = new TmxMapLoader().load("maps/towns/first_town/first-town.tmx");
+		var physicWorld = new World(Vector2.Zero, true);
+		physicWorld.setAutoClearForces(false);
 
-        this.physicWorld = new World(Vector2.Zero, true);
-        this.physicWorld.setAutoClearForces(false);
+		RenderSystem renderingSystem = new RenderSystem(new SpriteBatch(), gameCamera);
 
-        camera = new OrthographicCamera();
-        viewport = new ExtendViewport(WORLD_WIDTH, WORLD_HEIGHT, camera);
+		renderingSystem.setCurrentMap(map);
 
-        viewport.apply();
+		engine.addSystem(new InputSystem(this));
+		engine.addSystem(new MoveSystem());
+		engine.addSystem(new PhysicSystem(physicWorld, 1f / 60f));
+		engine.addSystem(new CameraSystem(gameCamera));
+		engine.addSystem(renderingSystem);
 
-        camera.position.set(camera.viewportWidth/2,camera.viewportHeight/2,0);
+		engine.addSystem(new PhysicsRenderSystem(physicWorld, gameCamera));
 
-        RenderSystem renderingSystem = new RenderSystem(sb, viewport, camera);
+		setScreen(new FirstScreen(engine, new CharacterCreator(physicWorld, engine)));
 
+		glProfiler = new GLProfiler(Gdx.graphics);
+		glProfiler.enable();
+		fpsLogger = new FPSLogger();
+	}
 
-        renderingSystem.setCurrentMap(map);
+	@Override
+	public void render() {
 
-        engine = new PooledEngine();
+		glProfiler.reset();
 
+		Gdx.gl.glClearColor(0, 0, 0, 1);
+		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 
-        engine.addSystem(new InputSystem(this));
-        engine.addSystem(new MoveSystem());
-        engine.addSystem(new PhysicSystem(physicWorld, 1f / 60f));
-        engine.addSystem(new CameraSystem(camera));
-        engine.addSystem(renderingSystem);
+		super.render();
 
-        engine.addSystem(new PhysicsRenderSystem(physicWorld, camera));
-
-        keyboardController = new KeyboardInputController(PlayTimeControllerState.class, engine, null);
-
-        inputMultiplexer.clear();
-
-        inputMultiplexer.addProcessor(keyboardController);
-
-        keyboardController.setActiveState(PlayTimeControllerState.class);
-
-        setScreen(new FirstScreen(engine, physicWorld));
-
-        glProfiler = new GLProfiler(Gdx.graphics);
-        glProfiler.enable();
-        fpsLogger = new FPSLogger();
-    }
-
-    @Override
-    public void render() {
-        glProfiler.reset();
-
-        Gdx.gl.glClearColor(0, 0, 0, 1);
-        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-
-        super.render();
-
-        Gdx.graphics.setTitle("Mystic Tutorial - Draw Calls: " + glProfiler.getDrawCalls());
-        fpsLogger.log();
-    }
+		Gdx.graphics.setTitle("Mystic Tutorial - Draw Calls: " + glProfiler.getDrawCalls());
+		fpsLogger.log();
+	}
 }
