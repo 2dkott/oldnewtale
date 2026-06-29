69bce40f521270539d6ec8221af535df4c380008
kot2d <kot2d@outlook.com>
2026-06-29 22:49:52 +0300

    add setter for region in TextureComponent

--- game-client/core/src/main/java/com/konivan/characters/Player.java
+++ game-client/core/src/main/java/com/konivan/characters/Player.java
@@ -4,15 +4,11 @@ import com.badlogic.ashley.core.Entity;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
-import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.physics.box2d.Body;
 import com.konivan.components.ControllerComponent;
 import com.konivan.components.MoveComponent;
 import com.konivan.components.PhysicComponent;
-import com.konivan.components.TextureComponent;
 import lombok.Getter;
-
-import java.util.concurrent.CopyOnWriteArrayList;
 
 @Getter
 public class Player extends Character {
@@ -22,7 +18,7 @@ public class Player extends Character {
         super(entity);
         var spriteSheet = new Texture(Gdx.files.internal("characters/main/test.png"));
         var region = new TextureRegion(spriteSheet, 0, 0, spriteSheet.getWidth(), spriteSheet.getHeight());
-        getTextureComponent().region = region;
+        getTextureComponent().setRegion(region);
         var moveComponent = new MoveComponent(4);
         var physicComponent = new PhysicComponent(body);
         var controllerComponent = new ControllerComponent();
--- game-client/core/src/main/java/com/konivan/components/TextureComponent.java
+++ game-client/core/src/main/java/com/konivan/components/TextureComponent.java
@@ -1,9 +1,15 @@
 package com.konivan.components;
 
 import com.badlogic.ashley.core.Component;
+import com.badlogic.ashley.core.ComponentMapper;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
+import lombok.Data;
+import lombok.Getter;
 
+@Data
 public class TextureComponent implements Component {
 
-    public TextureRegion region = null;
+    public static final ComponentMapper<TextureComponent> MAPPER = ComponentMapper.getFor(TextureComponent.class);
+
+    private TextureRegion region = null;
 }
