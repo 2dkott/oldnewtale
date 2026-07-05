96a44f5b5b64b8e9785c3a6fb9092c0032ae2cb0
kot2d <kot2d@outlook.com>
2026-07-05 20:56:39 +0300

    added loading screen

---
+++ game-client/core/src/main/java/com/konivan/screens/LoadingScreen.java
@@ -0,0 +1,48 @@
+package com.konivan.screens;
+
+import com.badlogic.gdx.Screen;
+import com.konivan.domain.assets.AssetService;
+import lombok.AllArgsConstructor;
+
+import javax.swing.*;
+
+@AllArgsConstructor
+public class LoadingScreen implements Screen {
+
+    private final AssetService assetService;
+
+    @Override
+    public void show() {
+
+    }
+
+    @Override
+    public void render(float delta) {
+
+    }
+
+    @Override
+    public void resize(int width, int height) {
+
+    }
+
+    @Override
+    public void pause() {
+
+    }
+
+    @Override
+    public void resume() {
+
+    }
+
+    @Override
+    public void hide() {
+
+    }
+
+    @Override
+    public void dispose() {
+
+    }
+}
