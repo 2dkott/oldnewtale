3a7c995a9258c1e01ae0c5dc3773dc2e2a77546b
kot2d <kot2d@outlook.com>
2026-09-02 16:45:56 +0300

    add godot

---
+++ tiledEditor/green_grass.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.12.1" name="green_grass" tilewidth="64" tileheight="64" tilecount="64" columns="8">
+ <image source="../game-client/assets/maps/nature/grass/green_grass.png" width="512" height="512"/>
+</tileset>
